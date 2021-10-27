540b642e7abda5a9ac3cfc5d102733ffa8378ae6
Jon Randy <jonrandy@gmail.com>
2021-10-27 16:43:45 +0700

    Adds 'curried/partial' code

--- metho-function.js
+++ metho-function.js
@@ -1,15 +1,35 @@
-//import * as Metho from "../metho/metho.js"
-import * as Metho from "metho"
+import * as Metho from "../metho/metho.js"
+//import * as Metho from "metho"
+
+const curryKey = Symbol('Curried function')
 
 const target = Function.prototype
 
 // Curried
 export const curried = Metho.add(
-  target,
-  function() {
-    return null
-  }
+	target,
+	function() {
+		return curry(this)
+	}
 )
 
 // Partial - alias for Curried
 export const partial = curried
+
+
+// generic currying function
+function curry(func) {
+	if (!func[curryKey]) {
+		console.log('curried created for ' + func.name)
+		func[curryKey] = function curried(...args) {
+			if (args.length >= func.length) {
+				return func.apply(this, args)
+			} else {
+				return function(...args2) {
+					return curried.apply(this, args.concat(args2))
+				}
+			}
+		}
+	}
+	return func[curryKey]
+}
