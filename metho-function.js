b26cf54c06a5ed18f76ed884524a410dfdb21977
Jon Randy <jonrandy@gmail.com>
2022-07-28 13:06:42 +0700

    Adds memoisation and refactor a bit

--- metho-function.js
+++ metho-function.js
@@ -1,7 +1,9 @@
 //import * as Metho from "../metho/metho.js"
 import * as Metho from "metho"
 
-const curryKey = Symbol('Curried function')
+const
+	curryKey = Symbol('Curried function'),
+	memoKey = Symbol('Memoised function')
 
 const target = Function.prototype
 
@@ -9,23 +11,41 @@ const target = Function.prototype
 export const curried = Metho.add(
 	target,
 	function curried() {
-		return curry(this)
+		return this[curryKey] || (this[curryKey]=curry(this))
 	}
 )
+
+// Memoised
+export const memoised = Metho.add(
+	target,
+	function memoised() {
+		return this[memoKey] || (this[memoKey]=memo(this))
+	}
+)
+export const memoized = memoised
 
 
 // generic currying function
 function curry(func) {
-	if (!func[curryKey]) {
-		func[curryKey] = function curried(...args) {
-			if (args.length >= func.length) {
-				return func.apply(this, args)
-			} else {
-				return function(...args2) {
-					return curried.apply(this, args.concat(args2))
-				}
+	return function curried(...args) {
+		if (args.length >= func.length) {
+			return func.apply(this, args)
+		} else {
+			return function(...args2) {
+				return curried.apply(this, args.concat(args2))
 			}
 		}
 	}
-	return func[curryKey]
+}
+
+// generic memoisation function
+function memo(func) {
+	const cache = new Map()
+	return function memoised(...args) {
+		const key = JSON.stringify(args)
+		if (cache.has(key)) return cache.get(key)
+		const result = func(...args)
+		cache.set(key, result)
+		return result
+	}
 }
